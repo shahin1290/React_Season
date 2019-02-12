9dd2e9334a134c57dc3a1a3a703732d6ba85e0db
shahin1290 <s.patowary@yahoo.com>
2019-02-12 19:29:47 +0100

    Finishes up the season app to display winter or summer based on geo location

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner'
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class App extends React.Component{
+    state = { lat: null, errorMessage: ''} 
+
+    componentDidMount(){
+        window.navigator.geolocation.getCurrentPosition(
+            (position)=> this.setState({ lat: position.coords.latitude}),
+            (err)=> this.setState({errorMessage: err.message})
+        )
+    }
+
+    renderContent(){
+        const {lat} = this.state
+        const {errorMessage} = this.state
+        if(!lat && errorMessage){
+            return <div>{errorMessage}</div>
+        }else if(lat && !errorMessage){
+            return <SeasonDisplay lat={lat} />
+        }else {
+            return <Spinner message="Please accept location request" />
+        }
+    }
+
+    render(){
+        
+        return (
+            <div>
+                { this.renderContent() }
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'))
